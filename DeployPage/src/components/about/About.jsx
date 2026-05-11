f7aaa9e164a610f0825b75e71180149e17a2c453
Hoss008 <43291152+Hoss008@users.noreply.github.com>
2026-05-11 19:48:17 +0300

    feat(About): Enhance About page layout and content
    - Added an image logo to the About section.
    - Introduced a new section highlighting services offered with a list format.
    - Updated CSS styles for better layout and responsiveness.
    - Included SVG icons for service items to improve visual appeal.
    - Created new SVG assets for the logo and arrow icons.

--- DeployPage/src/components/about/About.jsx
+++ DeployPage/src/components/about/About.jsx
@@ -1,15 +1,42 @@
 import styles from "../about/about.module.css";
+import aboutLogog from "../../assets/aboutLogo.svg";
+import Arrow from "../../assets/UI/Arrow.svg?react";
 
 function About() {
   return (
     <>
       <div className={styles["main-sec"]}>
-        <h1>NORQUE STUDIOS™</h1>
-        <p>
-          A MULTI DISCIPLINARY CREATIVE STUDIO THAT DELIVERS SOLUTIONS NOT JUST
-          GOOD DESIGNS
+        <img src={aboutLogog} alt="About Logo" />
+        <p className={styles["text"]}>
+          A MULTI DISCIPLINARY CREATIVE STUDIO THAT DELIVERS <br></br>
+          SOLUTIONS NOT JUST GOOD DESIGNS
         </p>
       </div>
+
+      <section className={styles.container}>
+        <div className={styles.emptyLeft}></div>
+
+        <div className={styles.contentRight}>
+          <h2 className={styles.heading}>
+            OUR
+            <br />
+            PROFESSION
+          </h2>
+          <p className={styles.subtitle}>WE ARE GOOD AT</p>
+
+          <ul className={styles.servicesList}>
+            <li>
+              <Arrow  /> UI/UX
+            </li>
+            <li>
+              <Arrow/> WEB DEVELOPMENT
+            </li>
+            <li>
+              <Arrow /> BRANDING
+            </li>
+          </ul>
+        </div>
+      </section>
     </>
   );
 }

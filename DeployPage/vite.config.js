38b33b920b37442b9be31d323c6517555d68c01f
Hoss008 <43291152+Hoss008@users.noreply.github.com>
2026-05-12 21:06:29 +0300

    style: format vite.config.js for consistency

--- DeployPage/vite.config.js
+++ DeployPage/vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    svgr()],
+  plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    target: "es2020", // or lower like 'es2015' for older browsers
+  },
+});

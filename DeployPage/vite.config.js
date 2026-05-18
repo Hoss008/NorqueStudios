dff61d8a9586fe91af1aa6208c51ba90ddb21b59
Hoss008 <43291152+Hoss008@users.noreply.github.com>
2026-05-18 20:49:59 +0300

    Implement structural updates and optimizations across multiple modules

--- DeployPage/vite.config.js
+++ DeployPage/vite.config.js
@@ -3,9 +3,19 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import svgr from "vite-plugin-svgr";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
